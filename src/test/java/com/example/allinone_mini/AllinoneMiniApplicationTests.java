264af08a6629f1570424e3d5ffa81fd26fc44aaa
tngh4037 <tngh4037@gmail.com>
2024-07-04 19:20:45 +0900

    project init

---
+++ src/test/java/com/example/allinone_mini/AllinoneMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allinone_mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllinoneMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
